3014ffae84d80d359c84c9036991f70d919dec24
Brooke <brookedunlap97@gmail.com>
2024-04-04 23:50:37 -0500

    Added ReadMe content. Added Modal with form. Filled out function to generate unique task ids. Partially worked on other functions.

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,37 +2,59 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    const timestamp = new Date().getTime();
+    const taskId = 'task_' + timestamp;
+    return taskId;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+  
 }
+
+
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+    
 }
+
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    //get inputs then check if inputs are blank, if not, then generate object for task, then you can pass that to creattaskcard
+    event.preventDefault();
 
+    const taskInput = $();
+    const tasDescription = taskInput.value;
+
+    if (!itaskDescription){
+        alert('Please do not leave the field blank');
+        return;
+    }
+
+    const newTask ={
+        id: generateTaskId(),
+        description: taskDescription,
+        completed:
+    }
 }
 
+    
 // Todo: create a function to handle deleting a task
 function handleDeleteTask(event){
-
+    
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+   
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    
 });
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # task-board
+
+## Description
+
+This program assists users with task management by allowing them to input their tasks and track their progress.
+
+## Installation
+
+N/A
+
+## Usage
+The user must click the "Add task" button and fill out the fields for task title, task deadline, and task description. Once that is done, a task card will be created on the page. The user can move the card into 3 different lanes: To Do, In Progress, Done. Tasks are able to be deleted by clicking the "Delete" button on the card.
+
+## Credits
+
+Starter code for this repo was provided by SMU for their coding bootcamp.
+
+Additional code provided by jQuery and Bootstrap.
+
+## License
+
+Please refer to the License in the repo.
+
+
